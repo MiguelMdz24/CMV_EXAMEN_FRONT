2a8bca8b44bacc16b35bed82f25e55dfa4f64d3e
Miguel Mdz <angelmendoza1451@gmail.com>
2022-12-19 20:17:29 -0600

    Implementacion bootstrap y pagina de error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorPageComponent } from './views/pages/error-page/error-page.component';
+
 
 const routes: Routes = [
   {
     path: '', loadChildren: () => import('./views/pages/clientes/clientes.module').then(m => m.ClientesModule)
+  },
+  {
+    path: '**', component: ErrorPageComponent,
   }
 ];
 
