905570a400b062a319db58b0959ea9217e1e4e0c
Miguel Mdz <angelmendoza1451@gmail.com>
2022-12-20 13:27:51 -0600

    Añadiendo el id de la cuenta del cliente

--- src/app/Models/cliente_cuenta.model.ts
+++ src/app/Models/cliente_cuenta.model.ts
@@ -1,4 +1,5 @@
 export interface Cliente_cuenta{
+    id_cliente_cuenta : number,
     nombre: String,
     nombre_cuenta: String,
     saldo_actual: number,
@@ -7,12 +8,14 @@ export interface Cliente_cuenta{
 }
 
 export class Cliente_cuentaModel implements Cliente_cuenta{
+    id_cliente_cuenta : number;
     nombre: String;
     nombre_cuenta: String;
     saldo_actual: number;
     fecha_contratacion: Date;
     fecha_ultimo_movimiento: Date;
     constructor(item: any){
+        this.id_cliente_cuenta = item.id_cliente_cuenta
         this.nombre = item.nombre
         this.nombre_cuenta = item.nombre_cuenta;
         this.saldo_actual = item.saldo_actual;
